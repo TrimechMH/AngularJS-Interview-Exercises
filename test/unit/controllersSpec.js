4c61353b99ee30a083713ea6d28f06518ed0f6d3
TrimechMH <trimech.med.habib@gmail.com>
2018-05-31 18:19:18 +0100

    After end of time write UT for update remove save spec.

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -32,13 +32,70 @@ describe('controllers', function () {
                 'phoneNumber': '53183633'
             };
             scope.addContact();
-            console.log(scope.contacts);
             expect(scope.contactModel).toEqual({});
             expect(scope.contacts).toEqual([{
                 'firstname': 'First name',
                 'lastname': 'last name',
                 'phoneNumber': '53183633'
             }]);
+        })
+    );
+
+    it('should update "contact" ',
+        inject(function () {
+            scope.contactModel = {
+                'firstname': 'First name',
+                'lastname': 'last name',
+                'phoneNumber': '53183633'
+            };
+            scope.addContact();
+            scope.contacts[0].firstname = 'updated!!';
+
+            expect(scope.contacts).toEqual([{
+                'firstname': 'updated!!',
+                'lastname': 'last name',
+                'phoneNumber': '53183633'
+            }]);
+        })
+    );
+
+    it('should remove "contact" ',
+        inject(function () {
+            scope.contacts = [{
+                'firstname': 'First name',
+                'lastname': 'last name',
+                'phoneNumber': '53183633'
+            },{
+                'firstname': 'TO BE',
+                'lastname': 'Removed',
+                'phoneNumber': '53183633'
+            }];
+            scope.removeContact( scope.contacts[1]);
+            console.log(scope.contacts);
+            expect(scope.contacts).toEqual([{
+                'firstname': 'First name',
+                'lastname': 'last name',
+                'phoneNumber': '53183633'
+            }]);
+        })
+    );
+
+
+    it('should save "contacts" ',
+        inject(function () {
+            scope.contacts = [{
+                'firstname': 'To BE ',
+                'lastname': 'Saved 1',
+                'phoneNumber': '53183633'
+            },{
+                'firstname': 'To BE ',
+                'lastname': 'Saved 2',
+                'phoneNumber': '53183633'
+            }];
+            scope.save();
+            console.log('localStorage', localStorage.getItem('contacts'));
+            expect(JSON.parse(localStorage.getItem('contacts'))).toEqual(scope.contacts);
+
         })
     );
 
